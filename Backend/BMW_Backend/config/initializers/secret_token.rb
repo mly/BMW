f5a3b405ac58d245ab5ce46a2b21634b05699101
Rob Balian <rbalian@stanford.edu>
2011-03-07 00:34:53 -0800

    Backend! Using rails. It doesn't do anything yet, but it will soon.

---
+++ Backend/BMW_Backend/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BMWBackend::Application.config.secret_token = '251d3110d4f92974ef1d25def4bdfd764fca5c4394be8e564d17f2d78dac86e4fb73c5773c67bc319c94892a8e851fd7ae44358900846fe33c4e99773d8392b1'

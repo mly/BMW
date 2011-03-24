a982372187f3236caaab0e5b6d2ae452b58acb89
John Jessen <benz.jessen@gmail.com>
2011-03-24 10:34:25 +0100

    added green light detection to shader

--- StoplightFiles/hsi_threshold.fs
+++ StoplightFiles/hsi_threshold.fs
@@ -1,9 +1,15 @@
-float H_MIN1 = 0.0;
-float H_MAX1 = 40.0;
-float H_MIN2 = 350.0;
-float H_MAX2 = 360.0;
-float S_THRESHOLD = .13;
+float RH_MIN1 = 0.0;
+float RH_MAX1 = 40.0;
+float RH_MIN2 = 350.0;
+float RH_MAX2 = 360.0;
+float RS_THRESHOLD = .13;
+
+float GH_MIN = 150.0;
+float GH_MAX = 180.0;
+float GS_THRESHOLD = .6;
+
 float I_THRESHOLD = 100.0;
+
 float RED_THRESHOLD = 200.0;
 float GY_THRESHOLD = 50.0;
 float PIXEL_THRESHOLD  = 50.0;
@@ -44,12 +50,20 @@ void main()
 	float sat = Saturation(pixel.r, pixel.g, pixel.b);
 	float hue = Hue(pixel.r, pixel.g, pixel.b);
 	
-	if (((hue >= H_MIN1 && hue <= H_MAX1) || (hue >= H_MIN2 && hue <= H_MAX2)) && sat >= S_THRESHOLD && ins >= I_THRESHOLD){
+	//Check for red traffic lights
+	if (((hue >= RH_MIN1 && hue <= RH_MAX1) || (hue >= RH_MIN2 && hue <= RH_MAX2)) && sat >= RS_THRESHOLD && ins >= I_THRESHOLD){
+	
 		if (pixel.r > RED_THRESHOLD && pixel.g < GY_THRESHOLD && pixel.b < GY_THRESHOLD) {
 			gl_FragColor = vec4(pixel.r, 0, 0, 1);
-		} else gl_FragColor = vec4(0,0,0,1);;
-	} else {
-		gl_FragColor = vec4(0,0,0,1);
+		} else gl_FragColor = vec4(0,0,0,1);
+
+	//Check for green traffic lights
+	} else if (hue >= GH_MIN && hue <= GH_MAX && sat >= GS_THRESHOLD && ins >= I_THRESHOLD){
+
+		if (pixel.g > RED_THRESHOLD && pixel.r < GY_THRESHOLD && pixel.b < GY_THRESHOLD) {
+			gl_FragColor = vec4(0, pixel.g, 0, 1);
+		} else gl_FragColor = vec4(0,0,0,1);	
+
 	}
 	
 }
